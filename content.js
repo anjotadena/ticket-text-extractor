e7c6771a1e6c5e74734193830193f32775e02019
Anjo Tadena <tadena.anjo@gmail.com>
2023-12-02 00:59:57 +0800

    Fix issue

--- content.js
+++ content.js
@@ -1,13 +1,34 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log(request, "REQUEST!");
     if (request.action === 'extractText') {
         const idField = document.querySelector('[aria-label="ID Field"]');
         const titleField = document.querySelector('[aria-label="Title Field"]');
-
-        const text = {
-            id: idField ? idField.textContent.trim() : 'Not found',
-            title: titleField ? titleField.textContent.trim() : 'Not found'
-        };
-
-        chrome.runtime.sendMessage({ action: 'displayText', text });
+        
+        if (idField && titleField) {
+            const text = `${idField.textContent.trim()}-${titleField.value.trim().toLowerCase().replace(/[^a-zA-Z0-9]/g, '-')}`;
+        
+            // Create a temporary textarea element to store the text
+            const textarea = document.createElement('textarea');
+            textarea.value = text;
+            document.body.appendChild(textarea);
+        
+            // Select the text in the textarea
+            textarea.select();
+            textarea.setSelectionRange(0, textarea.value.length);
+        
+            // Copy the selected text to the clipboard
+            document.execCommand('copy');
+        
+            // Remove the temporary textarea element
+            document.body.removeChild(textarea);
+        
+            // Log the copied text to the console
+            console.log(`Copied to clipboard: ${text}`);
+        
+            // Notify the background script or popup about the copied text
+            chrome.runtime.sendMessage({ action: 'displayText', text });
+          } else {
+            console.error('ID Field or Title Field not found');
+          }
     }
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 document.getElementById('extractBtn').addEventListener('click', () => {
+    console.log("GESS xx ");
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        console.log("SEND MESSAGE");
         chrome.tabs.sendMessage(tabs[0].id, { action: 'extractText' });
     });
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("GESS");
     if (request.action === 'displayText') {
         document.getElementById('result').textContent = request.text;
     }
